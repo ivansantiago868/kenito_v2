54c2b4412130422cf7126bd53b93280f8bbe5fdb
ivan del valle <ivansantiago868@hotmail.com>
2020-03-25 13:18:32 -0500

    login v1 ok con email y pass

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
-import 'pages/login_signup_page.dart';
+import 'package:kenito_v2/services/authentication.dart';
+import 'package:kenito_v2/pages/root_page.dart';
 
-void main() => runApp(new MyApp());
+void main() {
+  runApp(new MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-        title: 'Login',
+        title: 'Flutter login demo',
+        debugShowCheckedModeBanner: true,
         theme: new ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: new LoginSignupPage());
+        home: new RootPage(auth: new Auth()));
   }
 }
