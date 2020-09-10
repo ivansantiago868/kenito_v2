e214e6b89df154be86cdaa05fc6caa1cb05c078b
ivansantiago868 <ivansantiago868@gmail.com>
2020-09-09 22:02:47 -0500

    ajuste de acmbios por 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:kenito_v2/services/authentication.dart';
 import 'package:kenito_v2/pages/root_page.dart';
+import 'package:kenito_v2/pages/bd_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() {
   runApp(new MyApp());
@@ -9,12 +11,22 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // _signInAnonymously();
     return new MaterialApp(
         title: 'Flutter login demo',
         debugShowCheckedModeBanner: true,
         theme: new ThemeData(
           primarySwatch: Colors.blue,
         ),
+        // home: new BdPage());
         home: new RootPage(auth: new Auth()));
+  }
+
+  Future<void> _signInAnonymously() async {
+    try {
+      await FirebaseAuth.instance.signInAnonymously();
+    } catch (e) {
+      print(e); // TODO: show dialog with error
+    }
   }
 }
